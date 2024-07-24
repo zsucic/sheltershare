3d89f62c22666bd8dfda3d389f66559efcb7c6d9
zeljko_sucic <zeljko.sucic@2e-systems.com>
2024-07-24 17:22:02 +0200

    fix for websocket path

--- shshproj/shshapp/templates/shshapp/main.js
+++ shshproj/shshapp/templates/shshapp/main.js
@@ -1,5 +1,5 @@
 const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-const socket = new WebSocket(protocol + '//' + window.location.host + '/victim_request/');
+const socket = new WebSocket(protocol + '//' + window.location.host + '/shsh_ws/');
 function codeToClass(statusCode)
 {
     switch(statusCode)
